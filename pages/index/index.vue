fa1844d8c728f36f4370164caf52114d90942e1a
Flobby <2541226493@qq.com>
2020-12-04 08:12:18 +0800

    uni-app初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <view class="container">
+      <view v-for="(card,index) in cards" :key="index" class="cards">
+        <text>{{ card.title }}</text>
+        <image :src="card.bgImg" class="cover"/>
+        <text>{{card.content}}</text>
+      </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+		title: 'hello',
+        cards: [],  
+    };
+  },
+  onLoad(){
+	  uni.request({
+	  	url: 'http://localhost:8080/api/cards',
+		success: res => {
+			console.log(res.data.data);
+			this.cards = res.data.data;
+		}
+	  })
+  },
+  methods:{}
+}
+</script>
+
+<style scoped>
+.container {
+  width: 750rpx;
+  padding: 10rpx 10rpx;
+}
+
+.cards {
+  height: 500rpx;
+  width: 700rpx;
+  background-color: rgb(153,158,207);
+  padding: 20rpx;
+  margin-bottom: 20rpx;
+  border-radius: 10rpx;
+}
+.title{
+	font-size: 36rpx;
+}
+.cover {
+  width: 700rpx;
+  height: 300rpx;
+  border-radius: 10rpx;
+}
+
+</style>
